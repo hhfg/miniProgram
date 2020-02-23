e00e19ecf23b586b0da1b1fe63a9b6357a086e97
yonney <939964020@qq.com>
2020-02-23 19:39:04 +0800

    实现点击上下个按钮切换单词

--- pages/learning/learning.js
+++ pages/learning/learning.js
@@ -6,7 +6,9 @@ Page({
    * 页面的初始数据
    */
   data: {
-    words:'',         //将返回的对象存到word中
+    pos:0,
+    word:'',
+    words:[],         //将返回的对象存到word中
     ex_array:[],     //将释义分割字符串后存储在数组中
     coll_array:[]    //将词汇搭配分割字符串后存储在数组中
   },
@@ -38,6 +40,26 @@ Page({
       console.log(res.errCode)
     })
   },
+
+  previousWord: function () {
+    var that=this;
+    if(that.data.pos==0){
+      
+    }else{
+      that.setData({
+        pos:that.data.pos-1
+      })
+    }
+    that.onReady()
+  },
+  nextWord: function () {
+    var that=this;
+    that.setData({
+      pos:that.data.pos+1
+    })
+
+    that.onReady()
+  },
   /**
    * 生命周期函数--监听页面加载
    */
@@ -45,22 +67,18 @@ Page({
   onLoad: function (options) {
     var that=this;
     wx.request({
-      url: 'http://localhost:8080/MiniProgram/selWords.do',
+      //url: 'http://localhost:8080/MiniProgram/selWords.do',
+      url:'http://192.168.1.108:8080/MiniProgram/selWords.do',
       method:'GET',
       header:{
         'content-type':'application/json'
       },
+      // 返回数据成功，并将对象存到words中
       success:function(res){
         that.setData({
-          words:res.data,    
+          words:res.data,      
         })
-        that.setData({
-          ex_array: that.data.words.explanation.split(";")
-        })
-        that.setData({
-          coll_array:that.data.words.collocation.split(";")
-        })
-
+        console.log(that.data.word)  
       },
       fail:function(res){
         console.log("fail")
@@ -72,7 +90,19 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    var that = this
+    // 设置当前页面的单词
+    that.setData({
+      word: that.data.words[that.data.pos]
+    })
+    // 释义
+    that.setData({
+      ex_array: that.data.words[that.data.pos].explanation.split(";")
+    })
+    // 词汇搭配
+    that.setData({
+      coll_array: that.data.words[that.data.pos].collocation.split(";")
+    })
   },
 
   /**
