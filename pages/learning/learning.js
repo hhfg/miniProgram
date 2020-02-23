5cc0ea1f4e231ca84f68c2f7021a8ee4495c7fcc
yonney <939964020@qq.com>
2020-02-23 20:00:12 +0800

    修改bug
    点击下一个，如果单词是在最后一个会抛出错误，原因是没有判断当前单词是否是最后一个

--- pages/learning/learning.js
+++ pages/learning/learning.js
@@ -7,6 +7,7 @@ Page({
    */
   data: {
     pos:0,
+    len: 0,
     word:'',
     words:[],         //将返回的对象存到word中
     ex_array:[],     //将释义分割字符串后存储在数组中
@@ -49,16 +50,21 @@ Page({
       that.setData({
         pos:that.data.pos-1
       })
+      that.onReady()
     }
-    that.onReady()
+
   },
   nextWord: function () {
     var that=this;
-    that.setData({
-      pos:that.data.pos+1
-    })
-
-    that.onReady()
+    // 如果已经到最后一个单词
+    if(that.data.pos===(that.data.len-1)){
+      
+    }else{
+      that.setData({
+        pos: that.data.pos + 1
+      })
+      that.onReady()
+    }
   },
   /**
    * 生命周期函数--监听页面加载
@@ -76,9 +82,11 @@ Page({
       // 返回数据成功，并将对象存到words中
       success:function(res){
         that.setData({
-          words:res.data,      
+          words:res.data     
         })
-        console.log(that.data.word)  
+        that.setData({
+          len:res.data.length
+        })
       },
       fail:function(res){
         console.log("fail")
@@ -95,6 +103,7 @@ Page({
     that.setData({
       word: that.data.words[that.data.pos]
     })
+    console.log(that.data.word)
     // 释义
     that.setData({
       ex_array: that.data.words[that.data.pos].explanation.split(";")
