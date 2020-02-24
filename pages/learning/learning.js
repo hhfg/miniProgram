3bc85e868f7bf52d0cb280a84e691f2da884ce29
yonney <939964020@qq.com>
2020-02-24 14:10:49 +0800

    优化代码，修复bug

--- pages/learning/learning.js
+++ pages/learning/learning.js
@@ -1,5 +1,7 @@
 // pages/learning/learning.js
-
+const app = getApp();
+var startX, endX;
+var moveFlag = true;// 判断执行滑动事件
 Page({
 
   /**
@@ -10,9 +12,36 @@ Page({
     len: 0,
     word:'',
     words:[],         //将返回的对象存到word中
-    ex_array:[],     //将释义分割字符串后存储在数组中
-    coll_array:[]    //将词汇搭配分割字符串后存储在数组中
+    ex_array:[],      //将释义分割字符串后存储在数组中
+    coll_array:[],    //将词汇搭配分割字符串后存储在数组中
+
   },
+
+  touchStart: function (e) {
+    startX = e.touches[0].pageX; // 获取触摸时的原点
+    moveFlag = true;
+  },
+  // 触摸移动事件
+  touchMove: function (e) {
+    endX = e.touches[0].pageX; // 获取触摸时的原点
+    if (moveFlag) {
+      if (endX - startX > 50) {
+        this.previousWord();
+        moveFlag = false;
+      }
+      if (startX - endX > 50) {
+        this.nextWord();
+        moveFlag = false;
+      }
+    }
+
+  },
+  // 触摸结束事件
+  touchEnd: function (e) {
+    moveFlag = true; // 回复滑动事件
+  },
+
+
   /**
    * 生命周期函数--监听页面加载
    */
@@ -103,20 +132,14 @@ Page({
   pronouncePlayUS: function (event) {
     var url = event.currentTarget.dataset.url
     url = url.substring(1, url.length - 1)
-    const innerAudioContext = wx.createInnerAudioContext()
-    innerAudioContext.autoplay = true
-    innerAudioContext.src = url
-    innerAudioContext.onPlay(() => {
-      console.log('开始播放')
-    })
-    innerAudioContext.onError((res) => {
-      console.log(res.errMsg)
-      console.log(res.errCode)
-    })
+    this.play(url)
   },
   pronouncePlayUK: function (event) {
     var url = event.currentTarget.dataset.url
     url = url.substring(1, url.length - 1)
+    this.play(url)
+  },
+  play:function(url){
     const innerAudioContext = wx.createInnerAudioContext()
     innerAudioContext.autoplay = true
     innerAudioContext.src = url
@@ -133,6 +156,10 @@ Page({
     var that=this;
     if (that.data.pos == 0) {
     } else {
+      that.setData({
+        pos: that.data.pos - 1
+      })
+      console.log(that.data.pos)
       that.changeField();
     }
   },
@@ -141,17 +168,18 @@ Page({
     // 如果已经到最后一个单词
     var that=this;
     if (that.data.pos === (that.data.len - 1)) {
-      console.log('11')
+      
     } else {
+      that.setData({
+        pos: that.data.pos + 1
+      })
+      console.log(that.data.pos)
       that.changeField()
     }
   },
   // 修改单词字段
   changeField:function(){
     var that=this;
-    that.setData({
-      pos: that.data.pos + 1
-    })
     that.setData({
       word: that.data.words[that.data.pos]
     })
--- pages/test/test.js
+++ pages/test/test.js
@@ -1,85 +1,63 @@
-// pages/test/test.js
+
+// slide.js
+//获取应用实例
+const app = getApp();
+var startX, endX;
+var moveFlag = true;// 判断执行滑动事件
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    cardList: [
-      {
-        name: 'aa',
-        content: 'bb'
-      }, {
-        name: 'aa',
-        content: 'bb'
-      }, {
-        name: 'aa',
-        content: 'bb'
-      }, {
-        name: 'aa',
-        content: 'bb'
-      }, {
-        name: 'aa',
-        content: 'bb'
-      }, {
-        name: 'aa',
-        content: 'bb'
-      }, {
-        name: 'aa',
-        content: 'bb'
-      }, {
-        name: 'aa',
-        content: 'bb'
-      },
-    ]
+    content: "可以试试左右滑动了"
+  },
+  touchStart: function (e) {
+    startX = e.touches[0].pageX; // 获取触摸时的原点
+    moveFlag = true;
+  },
+  // 触摸移动事件
+  touchMove: function (e) {
+    endX = e.touches[0].pageX; // 获取触摸时的原点
+    if (moveFlag) {
+      if (endX - startX > 50) {
+        console.log("move right");
+        this.move2right();
+        moveFlag = false;
+      }
+      if (startX - endX > 50) {
+        console.log("move left");
+        this.move2left();
+        moveFlag = false;
+      }
+    }
+
+  },
+  // 触摸结束事件
+  touchEnd: function (e) {
+    moveFlag = true; // 回复滑动事件
+
+  },
+
+  move2left() {
+    var that = this;
+
+    that.setData({
+      content: "move2left"
+    });
+  },
+  move2right() {
+    var that = this;
+    that.setData({
+      content: "move2right"
+    });
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
 
   },
 
