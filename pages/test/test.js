c34c660f9b1d193c77e4f54376c66fe7b639e68f
yonney <939964020@qq.com>
2020-04-06 14:18:01 +0800

    完善等待对手界面

--- pages/test/test.js
+++ pages/test/test.js
@@ -5,71 +5,8 @@ Page({
    * 页面的初始数据
    */
   data: {
-    msg:'',
-    sendMsg:[],
-    socketOpen:false,
-    resData:[]
-  },
-  createConn:function(){
-    var page=this;
-    wx.connectSocket({
-      url: 'ws://localhost:8080/MiniProgram/getServer',
-      data:{
-        x:'',
-        y:''
-      },
-      hearder:{
-        'content-type':'Application/json'
-      },
-      method:'GET',
-    });
-    wx.onSocketOpen(function(res){
-      console.log(res);
-      page.setData({
-        socketOpen:true
-      })
-      console.log('WebSocket连接已打开')
-    });
-    wx.onSocketError(function(res){
-      console.log("WebSocket连接打开失败，请检查!")
-    })
-  },
-  send:function(e){
-    if(this.data.socketOpen){
-      console.log(this.data.socketOpen);
-      wx.sendSocketMessage({
-        data: this.data.msg,
-      })
-      var sendMsg=this.data.sendMsg;
-      sendMsg.push(this.data.msg);
-      this.setData({
-        sendMsg:sendMsg
-      })
-      var page=this;
-      wx.onSocketMessage(function(res){
-        var resData=page.data.resData;
-        resData.push(res.data);
-        page.setData({
-          resData:resData
-        })
-        console.log(resData);
-        console.log("收到服务器内容:"+res.data)
-      })
-    }else{
-      console.log("WebSocket连接打开失败，请检查")
-    }
-  },
-  closeConn:function(e){
-    wx.closeSocket();
-    wx.onSocketClose(function(res){
-      console.log('WebSocket已关闭')
-    })
-  },
-  getMsg:function(e){
-    var page=this;
-    page.setData({
-      msg:e.detail.value
-    })
+    waiting:false,
+    hidden:true
   },
   /**
    * 生命周期函数--监听页面加载
