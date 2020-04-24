bc70f6e76a84833ed8a230ef759b0935e44205f4
yonney <939964020@qq.com>
2020-04-24 23:13:32 +0800

    完善获取对战记录并显示场数和胜率

--- index/battleRecord/battleRecord.js
+++ index/battleRecord/battleRecord.js
@@ -1,20 +1,75 @@
 // index//battleRecord/battleRecord.js
+const app = getApp();
+const common = require("../../utils/common.js")
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    battleRecord:[],
+    count:0,
+    win:0,
+    winPercent:0
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    var that=this;
+    common.getData('selRecord.do',{
+      id:app.globalData.userData.uid
+    }).then((res)=>{
+      console.log(res.data)
+      that.setData({
+        battleRecord:res.data,
+        count:res.data.length
+      })
+      for (var i = 0; i < that.data.battleRecord.length; i++){
+        that.data.battleRecord[i].result=''
+      }
+      var num=0;
+      for(var i=0;i<that.data.battleRecord.length;i++){
+        if(that.data.battleRecord[i].playA==app.globalData.userData.uid){
+          if (that.data.battleRecord[i].ascore < that.data.battleRecord[i].bscore){
+            var result='battleRecord['+i+'].result'
+            that.setData({
+              [result]:'失败'
+            })
+          }else{
+            var result = 'battleRecord[' + i + '].result'
+            num++;
+            that.setData({
+              [result]: '胜利',
+              win:that.data.win+1,
+              winPercent: (num / that.data.count * 100).toFixed(2)
+            })
+          }
+        } else if (that.data.battleRecord[i].playB == app.globalData.userData.uid){
+          if (that.data.battleRecord[i].ascore < that.data.battleRecord[i].bscore) {
+            var result = 'battleRecord[' + i + '].result'
+            num++;
+            that.setData({
+              [result]: '胜利',
+              win: that.data.win+1,
+              winPercent:(num/that.data.count*100).toFixed(2)
+            })
+          } else {
+            var result = 'battleRecord[' + i + '].result'
+            that.setData({
+              [result]: '失败'
+            })
+          }
+        }
+      }
+    })
+    
+  },
+  getUserPic:function(){
+    var that=this;
 
   },
-
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
